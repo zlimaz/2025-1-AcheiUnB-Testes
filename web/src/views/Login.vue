ace7ba368e222dedbd5b6e32b77b7e4b271440d6
TiagoBalieiro <tiagoantunes0905@gmail.com>
2024-11-29 18:52:25 -0300

    Mudanças na tela de Login

--- web/src/views/Login.vue
+++ web/src/views/Login.vue
@@ -1,13 +1,27 @@
 <template>
-  <div class="bg-azul lg:bg-verde text-white p-4 h-screen">
-    <Logo />
-    <p>Perdeu algo no campus?</p>
-    <p>A gente te ajuda!</p>
-  </div>
+    <div class="telaInteira bg-azul text-white p-4 h-screen">
+      <div class="titulo flex space-x-1 mt-20 mb-20 ml-8 md:flex md:justify-center md:mb-52">
+        <h1 class="text-5xl italic font-light md:text-6xl">Achei</h1>
+        <h1 class="text-5xl font-bold md:text-6xl">UnB</h1>
+      </div>
+      <div class="slogan max-w-72 ml-8 md:mr-8 md:max-w-none md:w-auto md:text-center">
+        <p class="text-5xl font-bold mb-4 md:text-6xl">Perdeu algo no campus?</p>
+        <p class="text-5xl italic font-light mb-4 md:text-6xl">A gente te ajuda!</p>
+      </div>
+
+      <div class="flex justify-center mt-52 lg:">
+      <button class="flex items-center rounded-full bg-gray-50 px-5 py-3 text-md font-medium text-azul ring-1 ring-inset ring-gray-500/10">
+        <img src="../components/icons/Microsoft_logo.svg" alt="Logo Microsoft" class="h-6 w-auto mr-2"/>
+        Entre com a conta da Microsoft
+      </button>
+    </div>
+    </div>
 </template>
 
 <script setup>
-import Logo from "../components/Logo.vue";
+
 </script>
 
-<style scoped></style>
+<style scoped>
+
+</style>
