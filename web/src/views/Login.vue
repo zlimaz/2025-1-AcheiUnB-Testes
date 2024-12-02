34a5ed967c0bad0071f63b649eb0f18bf14a8c9a
TiagoBalieiro <tiagoantunes0905@gmail.com>
2024-12-02 14:21:51 -0300

    Alterações no CSS da página e no arquvio SVG da logo do botão.

--- web/src/views/Login.vue
+++ web/src/views/Login.vue
@@ -1,22 +1,22 @@
 <template>
-    <div class="telaInteira bg-azul text-white p-4 h-screen">
-      <div class="titulo flex space-x-1 mt-20 mb-20 ml-8 md:flex md:justify-center md:mb-52">
+    <div class="telaInteira flex flex-col justify-between bg-azul text-white h-screen">
+      <div class="titulo flex space-x-1 mt-20 ml-10 md:justify-center">
         <h1 class="text-5xl italic font-light md:text-6xl">Achei</h1>
         <h1 class="text-5xl font-bold md:text-6xl">UnB</h1>
       </div>
-      <div class="slogan max-w-72 ml-8 md:mr-8 md:max-w-none md:w-auto md:text-center">
-        <p class="text-5xl font-bold mb-4 md:text-6xl">Perdeu algo no campus?</p>
-        <p class="text-5xl italic font-light mb-4 md:text-6xl">A gente te ajuda!</p>
+      <div class="slogan max-w-72 mb-40 ml-10 md:max-w-none md:w-auto md:text-center">
+        <p class="text-5xl font-bold md:text-6xl">Perdeu algo no campus?</p>
+        <p class="text-5xl italic font-light md:text-6xl">A gente te ajuda!</p>
       </div>
 
-      <div class="flex justify-center mt-52 lg:">
-      <button 
-      @click="redirectToLoginMicrosoft"
-      class="flex items-center rounded-full bg-gray-50 px-5 py-3 text-md font-medium text-azul ring-1 ring-inset ring-gray-500/10">
-        <!--img src="../components/icons/Microsoft_logo.svg" alt="Logo Microsoft" class="h-6 w-auto mr-2"/-->
-        Entre com a conta da Microsoft
-      </button>
-    </div>
+      <div class="flex justify-center  mb-40">
+        <button 
+        @click="redirectToLoginMicrosoft"
+        class="flex items-center rounded-full bg-gray-50 px-5 py-3 text-md font-medium text-azul ring-1 ring-inset ring-gray-500/10">
+          <img src="../components/icons/Microsoft_logo2.svg" alt="Logo Microsoft" class="h-6 w-auto mr-2">
+          Entre com a conta da Microsoft
+        </button>
+      </div>
     </div>
 </template>
 
