587e80e9faade3ca4048064a60c0343326667581
Potatoyz908 <euller2005@gmail.com>
2024-12-25 17:48:55 -0300

    Refactor(Banco de dados): Banco de dados configurado para usar environment variables e implementação da hospedagem do Banco de dados online

--- API/AcheiUnB/settings.py
+++ API/AcheiUnB/settings.py
@@ -143,11 +143,11 @@ SOCIALACCOUNT_ADAPTER = "users.adapters.CustomSocialAccountAdapter"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "db",
-        "USER": "postgres",
-        "PASSWORD": "senha",
-        "HOST": "db",
-        "PORT": "5432",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
     }
 }
 
