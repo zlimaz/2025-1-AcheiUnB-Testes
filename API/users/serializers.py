747831c14a14a1b5c86c0d32ae9c3d37795b28e0
Potatoyz908 <euller2005@gmail.com>
2024-12-26 21:21:42 -0300

    Feat(migrations): Add Category model e atualiza Item model com color, status, e found_lost_date fields; implementa ItemImage model e serializers

--- API/users/serializers.py
+++ API/users/serializers.py
@@ -1,7 +1,35 @@
 from rest_framework import serializers
-from .models import Item
+from .models import Item, Category, ItemImage
+
+
+class ItemImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ItemImage
+        fields = ['id', 'image_url', 'item']
+        extra_kwargs = {'item': {'read_only': True}}
+
 
 class ItemSerializer(serializers.ModelSerializer):
+    images = ItemImageSerializer(many=True, read_only=True)  # Relacionamento com imagens
+
     class Meta:
         model = Item
-        fields = ['id', 'name', 'description', 'category', 'location', 'image', 'is_valuable', 'created_at']
+        fields = [
+            'id',
+            'name',
+            'description',
+            'category',
+            'location',
+            'color',
+            'is_valuable',
+            'status',
+            'found_lost_date',
+            'created_at',
+            'images',
+        ]
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id', 'name', 'category_id']
