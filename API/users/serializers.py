599375da7954cf85174e5087f0a3757361df2ada
Potatoyz908 <euller2005@gmail.com>
2024-12-29 02:23:35 -0300

    Feat(serializers): Adiciona ColorSerializer e BrandSerializer; atualiza ItemSerializer para incluir barcode, color e brand

--- API/users/serializers.py
+++ API/users/serializers.py
@@ -1,6 +1,15 @@
 from rest_framework import serializers
 from .models import Item, Category, ItemImage
 
+class ColorSerializer(serializers.Serializer):
+    class Meta:
+        model = Item
+        fields = ['id', 'name', 'color_id']
+    
+class BrandSerializer(serializers.Serializer):
+    class Meta:
+        model = Item
+        fields = ['id', 'name', 'brand_id']
 
 class ItemImageSerializer(serializers.ModelSerializer):
     class Meta:
@@ -11,16 +20,19 @@ class ItemImageSerializer(serializers.ModelSerializer):
 
 class ItemSerializer(serializers.ModelSerializer):
     images = ItemImageSerializer(many=True, read_only=True)  # Relacionamento com imagens
+    barcode = serializers.CharField(read_only=True)  # Código único do item
 
     class Meta:
         model = Item
         fields = [
             'id',
+            'barcode',
             'name',
             'description',
             'category',
             'location',
             'color',
+            'brand',
             'is_valuable',
             'status',
             'found_lost_date',
