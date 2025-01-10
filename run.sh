276d280a0fc64de7b4bbb57f1c991f0e55a1a21d
Potatoyz908 <euller2005@gmail.com>
2025-01-10 03:27:00 -0300

    feat(makefile): adiciona Makefile para automatizar comandos de execução e pull

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Saia imediatamente se algum comando falhar
-set -e
-
-echo "Entrando no diretório da API..."
-cd API/  # Navega para o diretório da API
-
-echo "Iniciando os serviços Docker..."
-sudo docker compose up --build  # Constrói e sobe os serviços do Docker
