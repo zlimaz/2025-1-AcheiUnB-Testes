110220a7aa314deddeffc135da792bd80c221b9d
TiagoBalieiro <tiagoantunes0905@gmail.com>
2025-01-20 18:41:11 -0300

    feat(items):  Torna a tela de itens perdidos e achados dinâmica.
    Agora os itens cadastrados no banco de dados aparecem separadamente na página de Achados e Perdidos com base no seu status: "found" ou "lost".
    
    Refs #123

--- web/src/components/Item-Card.vue
+++ web/src/components/Item-Card.vue
@@ -13,14 +13,14 @@
     <div class="h-[2px] w-1/4 bg-laranja mt-4"></div>
 
     <!--textos-->
-    <div class="text-azul font-bold font-inter mt-1">Carteira</div>
+    <div class="text-azul font-bold font-inter mt-1">{{name}}</div>
     <div class="flex items-start">
       <img
         src="../assets/icons/locale.svg"
         alt=""
         class="w-[15px] h-[15px] mr-1"
       />
-      <div class="text-azul font-inter text-sm">UED - S1</div>
+      <div class="text-azul font-inter text-sm">{{ location }}</div>
     </div>
     <span class="text-right font-inter font-bold text-xs text-cinza3 p-1">{{
       time
@@ -33,7 +33,9 @@ export default {
   name: "ItemCard",
   props: {
     image: String,
+    name: String,
     time: String,
+    location: String,
   },
 };
 </script>
