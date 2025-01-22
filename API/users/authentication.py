91baea3076b2fa836111bd52ec51d922b3e1c945
Potatoyz908 <euller2005@gmail.com>
2025-01-21 23:41:58 -0300

    Feat(auth): Adicionar suporte à autenticação via cabeçalho Authorization

--- API/users/authentication.py
+++ API/users/authentication.py
@@ -3,10 +3,20 @@ from rest_framework_simplejwt.authentication import JWTAuthentication
 
 class CookieJWTAuthentication(JWTAuthentication):
     def authenticate(self, request):
-        raw_token = request.COOKIES.get("access_token")  # Lê o token do cookie
-        if not raw_token:
-            return None  # Sem token, sem autenticação
 
+        raw_token = request.COOKIES.get("access_token")
+
+        if not raw_token:
+            raw_token = request.META.get("HTTP_AUTHORIZATION")
+            if raw_token:
+                if raw_token.startswith("Bearer "):
+                    raw_token = raw_token[7:]
+
+        if not raw_token:
+            return None
+
+        # Obtém o token validado
         validated_token = self.get_validated_token(raw_token)
         user = self.get_user(validated_token)
+
         return user, validated_token
