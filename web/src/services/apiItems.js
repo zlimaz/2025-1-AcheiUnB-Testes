fb71ffb7a74a26abfa9cd1eee46cde1ba654b351
Potatoyz908 <euller2005@gmail.com>
2025-01-26 00:16:02 -0300

    refactor(api): remover função obsoleta de busca de todos os itens
    Co-authored-by: leoramiroo <leonardoramiroalves@gmail.com>

--- web/src/services/apiItems.js
+++ web/src/services/apiItems.js
@@ -1,17 +1,7 @@
 import axios from 'axios';
-import api from '@/services/api';
 
 const API_BASE_URL = 'http://localhost:8000/api/items';
 
-export const fetchAllItems = async () => {
-  try {
-    const response = await api.get('/items');
-    return response.data.results; // Retorna apenas os resultados
-  } catch (error) {
-    console.error('Erro ao buscar itens:', error);
-    return [];
-  }
-}
 export const fetchLostItems = async (page = 1) => {
   const response = await axios.get(`${API_BASE_URL}/lost/`, {
     params: { page },
