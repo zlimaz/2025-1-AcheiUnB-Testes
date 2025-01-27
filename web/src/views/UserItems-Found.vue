6178b2d64b8a8491c7940e1467ce62b7e3e3193a
TiagoBalieiro <tiagoantunes0905@gmail.com>
2025-01-27 04:41:32 -0300

    fix(meus-itens): corrige erros da atribuição do resultado da API.
    Co-authored-by: Potatoyz908 <euller2005@gmail.com>
    Co-authored-by: Davicamilo23 <camilomenezesd@gmail.com>

--- web/src/views/UserItems-Found.vue
+++ web/src/views/UserItems-Found.vue
@@ -4,21 +4,21 @@
             <img src="../assets/icons/arrow-left-white.svg" alt="Voltar" class="w-[30px] h-[30px] text-white" />
         </router-link>
     </div>
+
     <div class="pb-8">
         <SubMenu />
     </div>
 
-    <div class="grid grid-cols-[repeat(auto-fit,_minmax(180px,_1fr))] sm:grid-cols-[repeat(auto-fit,_minmax(200px,_1fr))] justify-items-center align-items-center lg:px-3 gap-y-3 pb-10"
-            >
-            <ItemCard
-                v-for="item in myItemsFound"
-                :key="item.id"
-                :name="item.name"
-                :location="item.location_name"
-                :time="formatTime(item.created_at)"
-                :image="item.image_urls[0] || NotAvailableImage"
-            />
-        </div>
+    <div class="grid grid-cols-[repeat(auto-fit,_minmax(180px,_1fr))] sm:grid-cols-[repeat(auto-fit,_minmax(200px,_1fr))] justify-items-center align-items-center lg:px-3 gap-y-3 pb-10">
+        <ItemCard
+            v-for="item in myItemsFound"
+            :key="item.id"
+            :name="item.name"
+            :location="item.location_name"
+            :time="formatTime(item.created_at)"
+            :image="item.image_urls[0] || NotAvailableImage"
+        />
+    </div>
 
     <ButtonAdd />
     <div class="fixed bottom-0 w-full">
@@ -29,16 +29,19 @@
 <script setup>
 import { ref, onMounted } from 'vue';
 import { fetchMyItemsFound } from '@/services/apiItems';
+import { formatTime } from '@/utils/dateUtils';
 import MainMenu from "../components/Main-Menu.vue";
 import SubMenu from "../components/Sub-Menu-UserFound.vue";
+import ItemCard from '@/components/Item-Card.vue';
+import NotAvailableImage from '@/assets/images/not-available.png';
 
 const myItemsFound = ref([]);
+
 const fetchItems = async () => {
     const response = await fetchMyItemsFound();
-    myItemsFound.value = response.results;
+    myItemsFound.value = response;
 }
 onMounted(() => fetchItems());
-
 </script>
 
 <style scoped></style>
