86d5ed6a975c869e16e28487684e2d582b4e5769
Potatoyz908 <euller2005@gmail.com>
2025-01-29 00:42:47 -0300

    Fix(Migrations): Corrige erro ao iniciar projeto por conta de conflitos nas migrations

--- API/users/match.py
+++ API/users/match.py
@@ -6,6 +6,7 @@ def hamming_distance(barcode1: str, barcode2: str) -> int:
     """calcula a Hamming Distance entre dois barcodes"""
     return sum(c1 != c2 for c1, c2 in zip(barcode1, barcode2))
 
+
 def get_potential_matches(target_item: Item, opposite_status: str, max_distance: int):
     """Obtém itens com o status oposto ao do item alvo e calcula os matches."""
     potential_items = Item.objects.filter(
@@ -31,9 +32,7 @@ def generate_match_data(matches):
             "id": match.id,
             "name": match.name,
             "description": match.description,
-            "location": (
-                match.location.name if match.location else "Local não especificado"
-            ),
+            "location": (match.location.name if match.location else "Local não especificado"),
             "found_lost_date": (
                 match.found_lost_date.strftime("%d/%m/%Y")
                 if match.found_lost_date
@@ -48,7 +47,9 @@ def generate_match_data(matches):
 def find_and_notify_matches(target_item: Item, max_distance=2):
     """Encontra possíveis matches para o item fornecido e notifica o usuário."""
     if target_item.status == "lost":
-        matches = get_potential_matches(target_item, opposite_status="found", max_distance=max_distance)
+        matches = get_potential_matches(
+            target_item, opposite_status="found", max_distance=max_distance
+        )
         if matches:
             # Adicionar os matches ao item perdido
             target_item.matches.add(*matches)
@@ -63,7 +64,9 @@ def find_and_notify_matches(target_item: Item, max_distance=2):
             )
 
     elif target_item.status == "found":
-        potential_items = get_potential_matches(target_item, opposite_status="lost", max_distance=max_distance)
+        potential_items = get_potential_matches(
+            target_item, opposite_status="lost", max_distance=max_distance
+        )
         for lost_item in potential_items:
             # Adicionar o item encontrado como match ao item perdido
             lost_item.matches.add(target_item)
