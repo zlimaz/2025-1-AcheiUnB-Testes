fc85c7d7a039b319357f95016993a9784f6c0376
Potatoyz908 <euller2005@gmail.com>
2025-02-02 02:59:33 -0300

    feat(about): Atualiza a seção "Sobre" com informações dinâmicas dos criadores e melhorias de layout

--- web/src/views/About.vue
+++ web/src/views/About.vue
@@ -6,115 +6,39 @@
     <Logo />
   </div>
   <span
-    class="font-inter text-azul text-center flex justify-center items-center mx-auto max-w-3xl mt-7"
+    class="font-inter text-azul text-justify flex justify-center items-center mx-auto max-w-3xl sm:px-0 px-4 md:max-w-2xl mt-7"
   >
-    Somos um grupo da disciplina Métodos de Desenvolvimento de Software da Universidade de
-    Brasília.<br />
+  Somos um grupo da disciplina Métodos de Desenvolvimento de Software da Universidade de
+  Brasília.
     Temos como objetivo solucionar um dos problemas da faculdade, que é a falta de ferramentas para
-    organizar itens achados e perdidos pelos estudantes.<br />
-    Com o AcheiUnB você consegue de forma simples e intuitiva procurar pelo seu item perdido ou
+    organizar itens achados e perdidos pelos estudantes. Com o AcheiUnB você consegue de forma simples e intuitiva procurar pelo seu item perdido ou
     cadastrar um item que você encontrou.
   </span>
+  
+  <div class="flex justify-center mt-6">
+    <a
+      href="https://unb-mds.github.io/2024-2-AcheiUnB/CONTRIBUTING/"
+      target="_blank"
+      class="bg-azul text-white font-semibold px-6 py-2 rounded-full hover:bg-azulEscuro transition hover:text-yellow-400 duration-300 shadow-md rounded-full hover:scale-110 transition-transform"
+    >
+      Como Contribuir?
+    </a>
+  </div>
 
-  <div class="max-w-3xl mx-auto mt-10 p-5 pb-[140px]">
+  <div class="max-w-4xl mx-auto mt-10 p-5 pb-[140px]">
     <div class="text-azul text-lg text-center font-semibold mb-7">Criado por:</div>
 
-    <div class="space-y-3">
-      <div class="flex items-center justify-center space-x-4">
-        <a href="https://github.com/anaelisaramos" target="_blank">
+    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+      <div v-for="creator in creators" :key="creator.name" class="flex flex-col items-center">
+        <a :href="creator.github" target="_blank">
           <img
-            src="https://github.com/anaelisaramos.png"
-            alt="Ana Elisa Ramos"
+            :src="creator.image"
+            :alt="creator.name"
             class="w-[100px] rounded-full hover:scale-110 transition-transform"
           />
         </a>
-
-        <div class="text-left">
-          <p class="font-inter font-medium">Ana Elisa Ramos</p>
-          <p class="text-sm text-cinza3">Engenharia de Software</p>
-        </div>
-      </div>
-      <div class="flex items-center justify-center space-x-4">
-        <a href="https://github.com/DaviCamilo23" target="_blank">
-          <img
-            src="https://github.com/DaviCamilo23.png"
-            alt="Davi Camilo"
-            class="w-[100px] rounded-full hover:scale-110 transition-transform"
-          />
-        </a>
-
-        <div class="text-left">
-          <p class="font-inter font-medium">Davi Camilo</p>
-          <p class="text-sm text-cinza3">Engenharia de Software</p>
-        </div>
-      </div>
-      <div class="flex items-center justify-center space-x-4">
-        <a href="https://github.com/potatoyz908" target="_blank">
-          <img
-            src="https://github.com/potatoyz908.png"
-            alt="Euller Júlio da Silva"
-            class="w-[100px] rounded-full hover:scale-110 transition-transform"
-          />
-        </a>
-
-        <div class="text-left">
-          <p class="font-inter font-medium">Euller Júlio da Silva</p>
-          <p class="text-sm text-cinza3">Engenharia de Software</p>
-        </div>
-      </div>
-      <div class="flex items-center justify-center space-x-4">
-        <a href="https://github.com/leoramiroo" target="_blank">
-          <img
-            src="https://github.com/leoramiroo.png"
-            alt="Leonardo Ramiro"
-            class="w-[100px] rounded-full hover:scale-110 transition-transform"
-          />
-        </a>
-
-        <div class="text-left">
-          <p class="font-inter font-medium">Leonardo Ramiro</p>
-          <p class="text-sm text-cinza3">Engenharia de Software</p>
-        </div>
-      </div>
-      <div class="flex items-center justify-center space-x-4">
-        <a href="https://github.com/pedroeverton217" target="_blank">
-          <img
-            src="https://github.com/pedroeverton217.png"
-            alt="Pedro Everton"
-            class="w-[100px] rounded-full hover:scale-110 transition-transform"
-          />
-        </a>
-
-        <div class="text-left">
-          <p class="font-inter font-medium">Pedro Everton</p>
-          <p class="text-sm text-cinza3">Engenharia de Software</p>
-        </div>
-      </div>
-      <div class="flex items-center justify-center space-x-4">
-        <a href="https://github.com/314dro" target="_blank">
-          <img
-            src="https://github.com/314dro.png"
-            alt="Pedro Martins Silva"
-            class="w-[100px] rounded-full hover:scale-110 transition-transform"
-          />
-        </a>
-
-        <div class="text-left">
-          <p class="font-inter font-medium">Pedro Martins Silva</p>
-          <p class="text-sm text-cinza3">Engenharia de Software</p>
-        </div>
-      </div>
-      <div class="flex items-center justify-center space-x-4">
-        <a href="https://github.com/TiagoBalieiro" target="_blank">
-          <img
-            src="https://github.com/TiagoBalieiro.png"
-            alt="Tiago Balieiro"
-            class="w-[100px] rounded-full hover:scale-110 transition-transform"
-          />
-        </a>
-
-        <div class="text-left">
-          <p class="font-inter font-medium">Tiago Balieiro</p>
+        <div class="text-center mt-2">
+          <p class="font-inter font-medium">{{ creator.name }}</p>
           <p class="text-sm text-cinza3">Engenharia de Software</p>
         </div>
       </div>
@@ -131,6 +55,45 @@
 import AboutHeader from "../components/Header-About.vue";
 import MainMenu from "../components/Main-Menu.vue";
 import Logo from "../components/Logo.vue";
+
+
+const creators = [
+  {
+    name: "Ana Elisa Ramos",
+    github: "https://github.com/anaelisaramos",
+    image: "https://github.com/anaelisaramos.png"
+  },
+  {
+    name: "Davi Camilo",
+    github: "https://github.com/DaviCamilo23",
+    image: "https://github.com/DaviCamilo23.png"
+  },
+  {
+    name: "Euller Júlio da Silva",
+    github: "https://github.com/potatoyz908",
+    image: "https://github.com/potatoyz908.png"
+  },
+  {
+    name: "Leonardo Ramiro",
+    github: "https://github.com/leoramiroo",
+    image: "https://github.com/leoramiroo.png"
+  },
+  {
+    name: "Pedro Everton",
+    github: "https://github.com/pedroeverton217",
+    image: "https://github.com/pedroeverton217.png"
+  },
+  {
+    name: "Pedro Martins Silva",
+    github: "https://github.com/314dro",
+    image: "https://github.com/314dro.png"
+  },
+  {
+    name: "Tiago Balieiro",
+    github: "https://github.com/TiagoBalieiro",
+    image: "https://github.com/TiagoBalieiro.png"
+  }
+];
 </script>
 
 <style scoped></style>
