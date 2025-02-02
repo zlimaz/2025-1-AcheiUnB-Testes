06bed48e087332c11fe4d60c2055abdeed696557
Potatoyz908 <euller2005@gmail.com>
2025-02-02 18:10:22 -0300

    feat(about, user): Melhora a responsividade e o estilo das páginas "Sobre" e "Usuário"

--- web/src/views/About.vue
+++ web/src/views/About.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="relative min-h-screen">
-    <div 
+    <div
       class="absolute inset-0 bg-no-repeat bg-cover bg-center opacity-10 z-[-1]"
-      :style="{ 
-        backgroundImage: `url(${backgroundImage})`, 
-        backgroundSize: isLargeScreen ? '50%' : 'contain', 
-        backgroundAttachment: 'fixed' 
+      :style="{
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundSize: isLargeScreen ? '50%' : 'contain',
+        backgroundAttachment: 'fixed',
       }"
     ></div>
     <div class="fixed w-full top-0" style="z-index: 1">
@@ -15,26 +15,25 @@
       <Logo />
     </div>
     <span
-      class="font-inter text-azul text-center
-       flex justify-center items-center mx-auto max-w-3xl sm:px-0 px-4 md:max-w-2xl mt-7"
+      class="font-inter text-azul text-center flex justify-center items-center mx-auto max-w-3xl sm:px-0 px-4 md:max-w-2xl mt-7"
     >
       Somos um grupo da disciplina Métodos de Desenvolvimento de Software da Universidade de
       Brasília.<br />
-      Temos como objetivo solucionar um dos problemas da faculdade, que é a falta de ferramentas para
-      organizar itens achados e perdidos pelos estudantes.<br /> Com o AcheiUnB você consegue de forma simples e intuitiva procurar pelo seu item perdido ou
+      Temos como objetivo solucionar um dos problemas da faculdade, que é a falta de ferramentas
+      para organizar itens achados e perdidos pelos estudantes.<br />
+      Com o AcheiUnB você consegue de forma simples e intuitiva procurar pelo seu item perdido ou
       cadastrar um item que você encontrou.
     </span>
-    
-   <div class="flex justify-center mt-6">
-  <a
-    href="https://unb-mds.github.io/2024-2-AcheiUnB/CONTRIBUTING/"
-    target="_blank"
-    class="bg-laranja text-white font-semibold px-8 py-3 text-lg lg:text-xl rounded-full hover:bg-azulEscuro transition hover:text-white duration-300 shadow-md hover:scale-110 transition-transform"
-  >
-    Como Contribuir?
-  </a>
-</div>
 
+    <div class="flex justify-center mt-6">
+      <a
+        href="https://unb-mds.github.io/2024-2-AcheiUnB/CONTRIBUTING/"
+        target="_blank"
+        class="bg-laranja text-white font-semibold px-8 py-3 text-lg lg:text-xl rounded-full hover:bg-azulEscuro transition hover:text-white duration-300 shadow-md hover:scale-110 transition-transform"
+      >
+        Como Contribuir?
+      </a>
+    </div>
 
     <div class="max-w-4xl mx-auto mt-10 p-5 pb-[140px]">
       <div class="text-azul text-lg text-center font-semibold mb-7">Criado por:</div>
@@ -76,38 +75,38 @@ const creators = [
   {
     name: "Ana Elisa Ramos",
     github: "https://github.com/anaelisaramos",
-    image: "https://github.com/anaelisaramos.png"
+    image: "https://github.com/anaelisaramos.png",
   },
   {
     name: "Davi Camilo",
     github: "https://github.com/DaviCamilo23",
-    image: "https://github.com/DaviCamilo23.png"
+    image: "https://github.com/DaviCamilo23.png",
   },
   {
     name: "Euller Júlio da Silva",
     github: "https://github.com/potatoyz908",
-    image: "https://github.com/potatoyz908.png"
+    image: "https://github.com/potatoyz908.png",
   },
   {
     name: "Leonardo Ramiro",
     github: "https://github.com/leoramiroo",
-    image: "https://github.com/leoramiroo.png"
+    image: "https://github.com/leoramiroo.png",
   },
   {
     name: "Pedro Everton",
     github: "https://github.com/pedroeverton217",
-    image: "https://github.com/pedroeverton217.png"
+    image: "https://github.com/pedroeverton217.png",
   },
   {
     name: "Pedro Martins Silva",
     github: "https://github.com/314dro",
-    image: "https://github.com/314dro.png"
+    image: "https://github.com/314dro.png",
   },
   {
     name: "Tiago Balieiro",
     github: "https://github.com/TiagoBalieiro",
-    image: "https://github.com/TiagoBalieiro.png"
-  }
+    image: "https://github.com/TiagoBalieiro.png",
+  },
 ];
 </script>
 
