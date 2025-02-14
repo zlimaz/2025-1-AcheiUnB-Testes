06dfac77d39df3d89a17fdac8f6a10463583d3d9
Potatoyz908 <euller2005@gmail.com>
2025-02-14 02:59:03 -0300

    fix(forms): aumenta o tempo de espera antes de redirecionar após a publicação de itens

--- web/src/components/Form-Lost.vue
+++ web/src/components/Form-Lost.vue
@@ -409,7 +409,7 @@ export default {
         }
         setTimeout(() => {
           window.location.replace(`http://localhost:8000/#/lost`);
-        }, 1000);
+        }, 2050);
       } catch (error) {
         this.alertMessage = "Erro ao publicar item.";
         this.submitError = true;
